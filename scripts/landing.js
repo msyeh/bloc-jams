cfb7889a59d0a9fcb15cb77059afd64a8d1586f3
Miri Yeh <miriyeh@gmail.com>
2017-04-02 14:47:09 -0700

    Assignment 23 events: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,14 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(points) {
- 
-    var revealPoints = function(i) {
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translate Y(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
- 
-    for (var i=0; i < points.length; i++) {
-        revealPoints(i);
-    }
+var revealPoints = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translate Y(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+
+var animatePoints = function(pointArray) {
+    forEach(pointArray, revealPoints);
 };
 
 window.onload = function() {
