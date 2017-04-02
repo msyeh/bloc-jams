038e3361f0428636fbe7450a32381bf9e729b65f
Miri Yeh <miriyeh@gmail.com>
2017-04-02 15:14:27 -0700

    Checkpoint 24 collection view: completed

---
+++ scripts/collection.js
@@ -0,0 +1,27 @@
+var collectionItemTemplate =
+     '<div class="collection-album-container column fourth">'
+   + '  <img src="assets/images/album_covers/01.png"/>'
+   + '  <div class="collection-album-info caption">'
+   + '    <p>'
+   + '      <a class="album-name" href="album.html"> The Colors </a>'
+   + '      <br/>'
+   + '      <a href="album.html"> Pablo Picasso </a>'
+   + '      <br/>'
+   + '      X songs'
+   + '      <br/>'
+   + '    </p>'
+   + '  </div>'
+   + '</div>'
+   ;
+
+window.onload = function() {
+     // select element with "album-covers" class name
+     var collectionContainer = document.getElementsByClassName('album-covers')[0];
+     // assign empty string to the element's innerHTML property (this sets the element's content to clear)
+     collectionContainer.innerHTML = '';
+ 
+     // create a for loop that inserts 12 albums - each loop adds contents of album template to element's content
+     for (var i = 0; i < 12; i++) {
+         collectionContainer.innerHTML += collectionItemTemplate;
+     }
+ }
