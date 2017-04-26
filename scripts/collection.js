7cbd06b771f6b8c7aadc00d441f1f086a323e824
Miri Yeh <miriyeh@gmail.com>
2017-04-26 16:25:22 -0700

    Checkpoint 30 collection album views: completed

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,5 @@
-var collectionItemTemplate =
+var buildCollectionItemTemplate = function() {
+    var template =
      '<div class="collection-album-container column fourth">'
    + '  <img src="assets/images/album_covers/01.png"/>'
    + '  <div class="collection-album-info caption">'
@@ -13,15 +14,20 @@ var collectionItemTemplate =
    + '  </div>'
    + '</div>'
    ;
-
-window.onload = function() {
+    //wrap template in jQuery object
+    return $(template);
+};
+    
+$(window).load(function() {
      // select element with "album-covers" class name
-     var collectionContainer = document.getElementsByClassName('album-covers')[0];
-     // assign empty string to the element's innerHTML property (this sets the element's content to clear)
-     collectionContainer.innerHTML = '';
+     var $collectionContainer = $('.album-covers');
+     
+    // assign empty string to the element's innerHTML property (this sets the element's content to clear)
+     $collectionContainer.empty();
  
      // create a for loop that inserts 12 albums - each loop adds contents of album template to element's content
      for (var i = 0; i < 12; i++) {
-         collectionContainer.innerHTML += collectionItemTemplate;
+         var $newThumbnail = buildCollectionItemTemplate();
+         $collectionContainer.append($newThumbnail);
      }
- }
+ });
